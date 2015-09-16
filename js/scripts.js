6991e793d02202829599e3bd98112706cbbbb61d
brigham & clinton <student@epicodus.com>
2015-09-16 16:01:50 -0700

    the die rolls

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,50 @@
-function RollTotal() {
+
+// collects the value of each roll of the dice
+function Collector(total) {
   this.total = [];
 }
 
+// tallies up a roll's array
+Collector.prototype.rollTotal = function() {
+  var roll = this.total;
+  return eval(roll.join('+'));
+}
+
+// creates a player
+function Player(user, playerTotal) {
+  this.user = user;
+  this.playerTotal = []
+}
+
+// creates a player's total score
+Player.prototype.bank = function() {
+  var score = this.playerTotal;
+  return eval(score.join('+'));
+}
+
+// this is the Dice
+var dice = function() {
+  return Math.floor(Math.random() * 6) + 1;
+  dice = "";
+};
+
+// var valueCheck = function() {
+//   if (this === 1)
+//
+// }
 
 
+$(document).ready(function() {
 
+  $('#roll').click(function() {
+    var newRoll = dice();
 
+    $(".die").text(newRoll);
 
+    var bank = new Collector();
+    var newBank = bank.total.push(newRoll);
 
-// var dice = function() { return Math.random() * (6-1) + 1; dice = ""; }; dice().toFixed();
+    $(".piggy-bank").text(newBank);
+
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -1,25 +1,26 @@
-describe('RollTotal', function(){
+describe('Collector', function(){
   it("collects random numbers between 1 and 6", function(){
-    var testRollTotal = new RollTotal();
-    expect(testRollTotal.total).to.eql([]);
+    var testCollector = new Collector();
+    expect(testCollector.total).to.eql([]);
+  });
+
+  // it("", function() {
+  //   var testCollector = new Collector();
+  //   expect(testCollector.rollTotal()).to.equal(0);
+  // });
+});
+
+describe('Player', function() {
+  it("will create a new player", function() {
+    var testPlayer = new Player("Player 1");
+    expect(testPlayer.user).to.equal("Player 1");
+    expect(testPlayer.playerTotal).to.eql([]);
   });
 });
 
 
-
-
-
-
-
-
-
-
-
-
-// describe('Contact', function(){
-//   it("creates a new contact with the given specifications", function(){
-//     var testContact = new Contact("Jim", "Sith");
-//     expect(testContact.firstName).to.equal("Jim");
-//     expect(testContact.lastName).to.equal("Sith");
-//     expect(testContact.address).to.eql([]);
-//   });
+describe('dice', function(){
+  it("will select a number at random between 1 and 6", function() {
+    expect(dice()).to.equal(1)
+  });
+});
