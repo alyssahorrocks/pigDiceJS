cc2f59a6ce7e9cb3f9328f8da7827c09b4903c25
Alyssa Horrocks & Bunker <student@epicodus.com>
2015-09-17 12:17:37 -0700

    add the turn score to the total score when the player clicks hold

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
 
 // collects the value of each roll of the dice
 //AHB - This is turn score.
-function Collector(total) {
+function Collector(total, finalScore) {
   this.total = [];
+  this.finalScore = 0;
 }
 
-// tallies up a roll's array
 Collector.prototype.turnTotal = function() {
   // var total = eval((this.total).join('+'));
   var turnTotal = 0;
@@ -15,19 +15,16 @@ Collector.prototype.turnTotal = function() {
   return turnTotal;
 }
 
+Collector.prototype.addScore = function() {
+  this.finalScore = (this.finalScore) + (this.turnTotal());
+}
+
 // creates a player
 function Player(user, playerTotal) {
   this.user = user;
   this.playerTotal = []
 }
 
-// creates a player's total score
-Player.prototype.totalScore = function() {
-  var score = this.playerTotal;
-  return eval(score.join('+'));
-}
-
-// this is the Dice
 
 $(document).ready(function() {
 
@@ -44,7 +41,6 @@ $(document).ready(function() {
     var player1 = new Player(player1Name);
     var player2 = new Player(player2Name);
     var turn = new Collector();
-
     $(".player-1").text(player1.user);
     $(".player-2").text(player2.user);
 
@@ -58,6 +54,24 @@ $(document).ready(function() {
         $(".player1-turnScore").text(turn.turnTotal());
       }
 
+    $("#hold").click(function() {
+      turn.addScore();
+      $(".player1-piggy-bank").text(turn.finalScore);
+      turn.total = []
+      $(".player1-turnScore").empty();
+
+
+    //   var tempScore = 0;
+    //   if tempScore > 0(); {
+    //
+    //
+    //   } else {
+    //   var tempScore = turn.turnTotal()
+    //   $(".player1-piggy-bank").text(turn.totalScore());
+    //
+    // }
+
+    });
       //AHB - this creates new array to hold total score for each turn.
       //AHB - where is bank coming from here?
       //AHB - Need a constructor for 'bank'?
